7c287001d0f1337940409279a4ed4fb01e10f8d4
Zembi <bvb4ever1908@outlook.com>
2023-10-24 22:52:52 +0300

    24_10_2023 home -> work

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     // ELEMENTS THAT WILL INTERACT WITH GSAP CURSOR
     jQuery('p').attr('data-cursor-scale', 'scale');
     jQuery('img').attr('data-cursor-scale', 'lg-scale');
@@ -31,50 +31,43 @@ window.addEventListener("load", (event) => {
     //     }
     // });
 
-    const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: ".sect7",
-          start: "center bottom",
-          end: "+=1000",
-          scrub: 1,
-          pin: true,
-          markers: true
-        }
-      });
-      tl.to(".sect7 .word", {yPercent: 0 , duration: 1})
-
     const sections = gsap.utils.toArray("section");
     const lastIndex = sections.length - 1;
 
     sections.forEach((section, i) => {
         section._bg = section.querySelector(".bg");
         section._p = section.querySelector(".p");
-        
-        // Give the backgrounds some random images
-        section._bg.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight*2}?random=${i})`;
 
+        // Give the backgrounds some random images
+        section._bg.style.backgroundImage = `url(https://picsum.photos/${innerWidth}/${innerHeight * 2}?random=${i})`;
+
+
+        console.log(section._p.querySelectorAll('.word'));
         // Create a standalone ST instance, and use the progress value (0 - 1) to tween the timeline's progress
-        ScrollTrigger.create({ 
+        ScrollTrigger.create({
             trigger: section,
-            start: ()=> i == 0 ? "top top" : "top bottom", // The FIRST section will use a different start value than the rest
-            end: ()=> i == lastIndex ? "top top" : "bottom top", // The LAST section will use a different start value than the rest    
+            start: () => i == 0 ? "top top" : "top bottom", // The FIRST section will use a different start value than the rest
+            end: () => i == lastIndex ? "top top" : "bottom top", // The LAST section will use a different start value than the rest    
             onRefresh: self => { // onRefresh (so it gets reset upon resize), create a timeline that moves the h1 + bg vertically      
-                section._tl = gsap.timeline({paused:true, defaults:{ease:'none', overwrite:'auto'}}) 
-                    .fromTo(section._p, {
-                        y:()=> i == 0 ? 0 : (innerHeight/2)*1.5
+                section._tl = gsap.timeline({ paused: true, defaults: { ease: 'none', overwrite: 'auto' } })
+
+                    .fromTo(section._p.querySelectorAll('.word')[12], {
+                        y: 100,
+                        opacity: 0
                     }, {
-                        y:()=> i==lastIndex ? 0 : (-innerHeight/2)*1.5
-                    }, 0)
+                        y: 0,
+                        opacity: 1
+                    }, 1)
                     .fromTo(section._bg, {
-                        y:()=> i==0 ? -innerHeight/2 : 0
+                        y: () => i == 0 ? -innerHeight / 2 : 0
                     }, {
-                        y:()=> i==lastIndex ? -innerHeight/2 : -innerHeight
-                    }, 0)    
-                    //.progress(self.progress); //use progress to position the timeline correctly      
+                        y: () => i == lastIndex ? -innerHeight / 2 : -innerHeight
+                    }, 0)
+                //.progress(self.progress); //use progress to position the timeline correctly      
             },
-            onUpdate: self => { gsap.to(section._tl, {duration:0.75, progress:self.progress}); }
+            onUpdate: self => { gsap.to(section._tl, { duration: 0.75, progress: self.progress }); }
         });
-    
+
     });
 
     // tl.from($('.sect8 .word'), {opacity: 0, y: 100, duration: 0.5, ease: 'back.out(2)', stagger: {amount: 2},
@@ -96,7 +89,7 @@ class ActivateGSAPCursor {
     constructor(cursorName = null) {
         // IF TRUE, EVERY CUSTOM CONSOLE LOG MESSAGE WILL BE SHOWN. IF FALSE WE CAN DISABLE ALL MESSAGES THAT HELP WITH THE CONSTRUCTOR
         this.consoleMsgsEnabled = true;
-        
+
         // CURSOR
         this.cursorName = cursorName;
         this.cursor = this.cursorName && document.querySelector(cursorName);
@@ -110,7 +103,7 @@ class ActivateGSAPCursor {
 
     #render() {
         // IF THE CURSOR IS BEING PASSED TO CONSTRUCTOR AND EXISTS
-        if(this.cursorName && this.cursor) {
+        if (this.cursorName && this.cursor) {
             this.#activateCursorEvents();
         }
         else {
@@ -122,11 +115,11 @@ class ActivateGSAPCursor {
     // -------------------------- CURSOR EVENTS --------------------------
     #activateCursorEvents() {
         // INITIATE CURSOR ACTION
-        gsap.set(this.cursor, { 
-            xPercent: 0, yPercent: 0, opacity: 1 
+        gsap.set(this.cursor, {
+            xPercent: 0, yPercent: 0, opacity: 1
         });
 
-        
+
         // ADJUST SPEED FOR HIGHER REFRESH MONITORS
         const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
         const mouse = { x: pos.x, y: pos.y };
@@ -138,11 +131,11 @@ class ActivateGSAPCursor {
             mouse.x = e.x;
             mouse.y = e.y;
         });
-        
+
         gsap.ticker.add(() => {
             const speed = 0.35;
             const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
-    
+
             pos.x += (mouse.x - pos.x) * dt;
             pos.y += (mouse.y - pos.y) * dt;
             xSet(pos.x);
@@ -173,8 +166,8 @@ class ActivateGSAPCursor {
         const currentScale = item.getAttribute(nameOfDataAttr);
 
         let scaleVal = 1;
-        mapScales.forEach(function(value, key) {
-            if(currentScale === key) {
+        mapScales.forEach(function (value, key) {
+            if (currentScale === key) {
                 scaleVal = parseInt(value);
             }
         });
@@ -192,7 +185,7 @@ class ActivateGSAPCursor {
         item.classList.add('hovered');
     }
 
-    #addVideoToCursor({videoUrl, imgUrl}) {
+    #addVideoToCursor({ videoUrl, imgUrl }) {
         this.additions = document.createElement('video');
 
         this.additions.src = videoUrl;
@@ -253,17 +246,17 @@ class GSAPTransition {
 
         gsap.from(".sect2 img", {
             scrollTrigger: {
-              trigger: ".sect1 img",
-              scrub: true,
-              start: "left",
-              end: "right"
+                trigger: ".sect1 img",
+                scrub: true,
+                start: "left",
+                end: "right"
             },
             scaleX: 0,
-            transformOrigin: "left center", 
+            transformOrigin: "left center",
             ease: "none"
         });
-          
-        
+
+
         gsap.from(".sect1 .line", {
             scrollTrigger: {
                 trigger: ".sect1",
@@ -272,29 +265,29 @@ class GSAPTransition {
                 start: "top 10%",
                 end: "+=100%"
             },
-            scaleX: 0, 
-            transformOrigin: "left center", 
+            scaleX: 0,
+            transformOrigin: "left center",
             ease: "none"
         });
 
         const tl = gsap.timeline({
             scrollTrigger: {
-              scrub: 1,
-              pin: true,
-              trigger: ".sect1 p",
-              start: "top 10%",
-              endTrigger: ".sect1",
-              end: "bottom 50%",
+                scrub: 1,
+                pin: true,
+                trigger: ".sect1 p",
+                start: "top 10%",
+                endTrigger: ".sect1",
+                end: "bottom 50%",
             },
         });
-          
+
         // tl.to(".sect1 p", {
         //     rotateX: 14400,
         // });
 
         var timeline = new TimelineMax();
-        timeline.from(".sect2", 1, {x: '-100%'}, 0)
-            .from(".sect1", 1, {x: '-100%'}, 0);
+        timeline.from(".sect2", 1, { x: '-100%' }, 0)
+            .from(".sect1", 1, { x: '-100%' }, 0);
 
     }
 
@@ -322,10 +315,10 @@ class SplitText {
     }
 
     #main() {
-        if(this.queryEl.length > 0) {
+        if (this.queryEl.length > 0) {
             this.#fixArray();
             this.typesOfSplit.forEach((item, index) => {
-                switch(item.toLocaleLowerCase()) {
+                switch (item.toLocaleLowerCase()) {
                     case 'char':
                         console.log(1);
                         this.charsActivated = true;
@@ -340,7 +333,7 @@ class SplitText {
                         this.sentencesActivated = true;
                         this.#splitSentences(this.#getCurrentClassName(index));
                         break;
-                    default :
+                    default:
                         break;
                 }
             });
@@ -351,19 +344,19 @@ class SplitText {
     #fixArray() {
         const uniqueStrings = new Set([...this.typesOfSplit]);
         this.typesOfSplit = [...uniqueStrings];
-        
+
         this.typesOfSplit = [...this.typesOfSplit.map(type => type.toLowerCase())];
 
         const newArr = [];
-        if(this.typesOfSplit.includes('char')) {
+        if (this.typesOfSplit.includes('char')) {
             newArr.push('char');
         }
 
-        if(this.typesOfSplit.includes('word')) {
+        if (this.typesOfSplit.includes('word')) {
             newArr.push('word');
         }
 
-        if(this.typesOfSplit.includes('sentence')) {
+        if (this.typesOfSplit.includes('sentence')) {
             newArr.push('sentence');
         }
 
@@ -373,7 +366,7 @@ class SplitText {
     #getCurrentClassName(index) {
         let result = null;
 
-        if(this.classesOfSplit.length > 0) {
+        if (this.classesOfSplit.length > 0) {
             result = this.classesOfSplit[index] ? this.classesOfSplit[index] : null;
         }
 
@@ -386,16 +379,16 @@ class SplitText {
 
         [...this.queryEl].forEach((el) => {
             let result = '';
-            
+
             let str = el.innerHTML;
-            
-            if(act) {
+
+            if (act) {
                 const allChars = str.split(/(?=[^>]*(?:<|$))/);
                 allChars.forEach((char, index) => {
                     let lastSpace = allChars[index + 1] === ' ' ? ' ' : '';
-                    
-                    if(!validateHTML(char)) {
-                        if(char !== ' ') {
+
+                    if (!validateHTML(char)) {
+                        if (char !== ' ') {
                             result += '<span class="' + className + '">' + char + '</span>' + lastSpace;
                         }
                         else {
@@ -412,7 +405,7 @@ class SplitText {
                 allChars.forEach((char, index) => {
                     let lastSpace = allChars[index + 1] === ' ' ? ' ' : '';
 
-                    if(char == ' ') {
+                    if (char == ' ') {
                         // console.log(1);
                         result += '<span class="' + this.emptySpacesClass + '">' + char + '</span>' + lastSpace;
                     }
@@ -421,7 +414,7 @@ class SplitText {
                     }
                 });
             }
-        
+
             el.innerHTML = result;
         });
     }
@@ -430,31 +423,31 @@ class SplitText {
         className = className ? className : 'gsapcustom-word';
         console.log('words', className);
 
-        if(!this.charsActivated) {
+        if (!this.charsActivated) {
             this.#splitChars('', false);
-        } 
+        }
 
         [...this.queryEl].forEach((el) => {
 
-            const charsAct = this.charsActivated;     
-            const emptyClass = this.emptySpacesClass;   
+            const charsAct = this.charsActivated;
+            const emptyClass = this.emptySpacesClass;
             el.innerHTML = wrapWords(el);
 
-            function wrapWords(elem) {  
+            function wrapWords(elem) {
                 let result = '';
                 let str = elem.innerHTML;
                 const allWords = str.split('<span class="' + emptyClass + '"> </span>');
                 // console.log(allWords);
                 allWords.forEach((word, index) => {
                     let lastSpace = index === (allWords.length - 1) ? '' : ' ';
-                    
-                    if(!validateHTML(word)) {
-                        if(word !== '' && word !== ' ') {
+
+                    if (!validateHTML(word)) {
+                        if (word !== '' && word !== ' ') {
                             result += '<span class="' + className + '">' + word + '</span>' + lastSpace;
                         }
                     }
                     else {
-                        if(charsAct) {
+                        if (charsAct) {
                             result += '<span class="' + className + '">' + word + '</span>' + lastSpace;
                         }
                         else {
@@ -465,7 +458,7 @@ class SplitText {
                         }
                     }
                 });
-            
+
                 return result;
             }
         });
@@ -481,15 +474,15 @@ class SplitText {
     #clearAllEmptyElements() {
         [...this.queryEl].forEach((el) => {
             const empties = el.querySelectorAll(`.${this.emptySpacesClass}`);
-            
-            for(const empt of empties) {
+
+            for (const empt of empties) {
                 empt.remove();
             }
         });
     }
 }
 
-function validateHTML(htmlString){
+function validateHTML(htmlString) {
     // console.log(htmlString);
     const regex = RegExp.prototype.test.bind(/(<([^>]+)>)/i);
     return regex(htmlString);
